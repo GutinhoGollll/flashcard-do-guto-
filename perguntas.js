ca5e930d4a14e9e0a77c0217b65bf5a439e5b0f5
GutinhoGollll <161051799+GutinhoGollll@users.noreply.github.com>
2024-10-16 11:54:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'FORMULA 1 ',
+    'Quem é o melhor piloto?',
+    'MAX VERSTAPPEN'
+)
+
+criaCartao(
+    'futebol',
+    'Quem é o maior da historia?',
+    'CR7 siuuuuuuuuuuuuuu'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se se elogia uma mulher em ingles?',
+    'gurl, your eyes are so pretty for me they look like stars, be my wife PLSSSS'
+)
