b9e306a58b2b43a90f17fd87c1417706e1083dce
GutinhoGollll <161051799+GutinhoGollll@users.noreply.github.com>
2024-10-23 08:02:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Dinheiro',
+    'ser humano mais rico no mundo',
+    'ELON MUSK'
+)
+
+criaCartao(
+    'PIADA',
+    'Seja como um próton.',
+    'Sempre seja positivo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'como chama um amigo em ingles',
+    'whats up my nigga'
+)
